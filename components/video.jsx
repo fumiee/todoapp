961c30112f7ad52899b4c0292b5f5bc847ef9916
fumiee <fumie.yori@gmail.com>
2021-07-05 22:10:28 +0900

    loadMoreを追加

--- components/video.jsx
+++ components/video.jsx
@@ -6,6 +6,7 @@ const BASE_URL = `https://www.youtube.com/embed/`;
 
 export const Video = (props) => {
   const [texts, setTexts] = useState([]);
+  const [pages, setPages] = useState();
 
   const fetchData = async () => {
     try {
@@ -15,8 +16,9 @@ export const Video = (props) => {
         { id: videoIds.join(","), part: "snippet,id" },
         "videos"
       );
-      setTexts(videoData);
-      console.log(videoData);
+      setPages(searchData.nextPageToken);
+      setTexts([...texts, ...videoData.items]);
+      console.log(searchData.nextPageToken);
     } catch (error) {
       console.error(error);
     }
@@ -26,9 +28,30 @@ export const Video = (props) => {
     fetchData();
   }, []);
 
+  const loadMore = async () => {
+    try {
+      const pageToken = pages;
+      const searchData = await callApi(
+        { ...props.params, pageToken },
+        props.req
+      );
+      const videoIds = searchData?.items.map((item) => item.id.videoId);
+      const videoData = await callApi(
+        { id: videoIds.join(","), part: "snippet,id" },
+        "videos"
+      );
+      setTexts([...texts, ...videoData.items]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log();
+
   return (
     <div className="space-y-4">
-      {texts.items?.map((item) => {
+      {texts.map((item) => {
+        console.log(item);
+
         return (
           <div key={item.etag} className="border-8 border-gray-400 rounded-xl">
             <div className=" bg-gray-400 text-white border-b-4 border-gray-400">
@@ -44,7 +67,6 @@ export const Video = (props) => {
             </div>
             <details className="text-sm pt-2 bg-gray-400 text-white">
               <summary className="list-none bg-gray-400">
-                {item.snippet.description.slice(0, 50)}
                 <div className="flex justify-between">
                   <div className="text-gray-300 text-sm flex items-end">
                     ▼概要欄を表示
@@ -52,11 +74,17 @@ export const Video = (props) => {
                   <Button />
                 </div>
               </summary>
-              <div>{item.snippet.description.slice(50, 10000)}</div>
+              <div className="break-all">{item.snippet.description}</div>
             </details>
           </div>
         );
       })}
+      <button
+        className="text-gray-400 flex justify-center items-center"
+        onClick={loadMore}
+      >
+        もっと見る
+      </button>
     </div>
   );
 };
